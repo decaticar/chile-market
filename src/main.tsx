bc4d607b0d0573df7e57fbfe1dc71703d5dde4ab
Deka Ticar <149799535+decaticar@users.noreply.github.com>
2025-12-04 21:59:58 -0300

    prue

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ChileMarketTracker from "./chile-market-tracker";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChileMarketTracker />
+  </React.StrictMode>
+);
